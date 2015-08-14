52ddc42b6b59f02c9a49ef9b4edcd2df890d2a8f
Karsten Knuth <karsten_knuth@gmx.net>
2015-08-14 22:16:43 +0200

    fixed spelling mistake

--- codility/src/main/java/org/ck/codility/challenges/alpha2010/Solution.java
+++ codility/src/main/java/org/ck/codility/challenges/alpha2010/Solution.java
@@ -3,7 +3,7 @@ package org.ck.codility.challenges.alpha2010;
 import java.util.HashSet;
 import java.util.Set;
 
-@org.ck.codeChallengeLib.annotation.Solution(id = 1001, name = "Alphy 2010 (PrefixSet)", url = "https://codility.com/programmers/challenges/alpha2010", category = "Challenges", subCategory = "Greeks")
+@org.ck.codeChallengeLib.annotation.Solution(id = 1001, name = "Alpha 2010 (PrefixSet)", url = "https://codility.com/programmers/challenges/alpha2010", category = "Challenges", subCategory = "Greeks")
 class Solution
 {
 	public int solution(int[] A)

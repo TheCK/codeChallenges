4f40c9ca8a17d02cb520757315f02b4ba3d5977a
TheCK <karsten_knuth@gmx.net>
2017-09-05 22:29:22 +0200

    added section for spoj

---
+++ spoj/generatedReadme.md
@@ -0,0 +1,12 @@
+#  (1/1)
+
+|   # | Name                     | Solution                        | Test                         |
+|----:|--------------------------|:-------------------------------:|:----------------------------:|
+| 101 | [A. Theatre Square][101] | &#9989;[&#128190;][101solution] | &#9989;[&#128190;][101tests] |
+
+[101]: http://codeforces.com/problemset/problem/1/A
+
+[101solution]: src/main/java/org/ck/spoj/classical/test/Main.java
+
+[101tests]: src/test/java/org/ck/spoj/classical/theatresquare/MainTest.java
+
---
+++ spoj/src/main/java/org/ck/spoj/classical/test/Main.java
@@ -0,0 +1,29 @@
+package org.ck.spoj.classical.test;
+
+import org.ck.codeChallengeLib.annotation.Solution;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Solution(
+		id = 1000001,
+		name = "TEST - Life, the Universe, and Everything",
+		url = "http://www.spoj.com/problems/TEST/",
+		category = "classical"
+)
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+		try (Scanner in = new Scanner(System.in))
+		{
+			int number = in.nextInt();
+			while (number != 42)
+			{
+				System.out.println(number);
+
+				number = in.nextInt();
+			}
+		}
+	}
+}

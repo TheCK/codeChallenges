d3eb54f112ea8c6ce72ca80ce78729945f158bc3
TheCK <karsten_knuth@gmx.net>
2017-09-06 17:10:37 +0200

    added first twenty test cases for codeforces 1C

---
+++ codeforces/src/main/java/org/ck/codeforces/ancientberlandcircus/Main.java
@@ -0,0 +1,24 @@
+package org.ck.codeforces.ancientberlandcircus;
+
+import org.ck.codeChallengeLib.annotation.Solution;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Solution(
+		id = 103,
+		name = "C. Ancient Berland Circus",
+		url = "http://codeforces.com/problemset/problem/1/C",
+		category = "1",
+		solved = false
+)
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+		try (Scanner in = new Scanner(System.in))
+		{
+
+		}
+	}
+}

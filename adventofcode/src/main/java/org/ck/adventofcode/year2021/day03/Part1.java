2f98800d34280891752fa95a8c301898c2ecba70
TheCK <hub@theck.dev>
2021-12-03 18:14:10 +0100

    day 3

---
+++ adventofcode/src/main/java/org/ck/adventofcode/year2021/day03/Part1.java
@@ -0,0 +1,48 @@
+package org.ck.adventofcode.year2021.day03;
+
+import org.ck.codeChallengeLib.annotation.Solution;
+
+import java.util.Scanner;
+
+@Solution(
+    id = 20210301,
+    name = "Day 3: Binary Diagnostic",
+    url = "https://adventofcode.com/2021/day/3",
+    category = "2021")
+public class Part1 {
+  public static void main(String[] args) {
+    int ones[] = null;
+    int count = 0;
+
+    try (Scanner in = new Scanner(System.in)) {
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+
+        ++count;
+
+        if (ones == null) {
+          ones = new int[line.length()];
+        }
+
+        for (int i = 0; i < line.length(); ++i) {
+          if (line.charAt(i) == '1') {
+            ++ones[i];
+          }
+        }
+      }
+    }
+
+    int gamma = 0;
+    int epsilon = 0;
+
+    for (int i = 0; i < ones.length; ++i) {
+      if (ones[i] > (count / 2)) {
+        gamma |= 1 << (ones.length - i - 1);
+      } else {
+        epsilon |= 1 << (ones.length - i - 1);
+      }
+    }
+
+    System.out.println(gamma * epsilon);
+  }
+}
---
+++ adventofcode/src/main/java/org/ck/adventofcode/year2021/day03/Part2.java
@@ -0,0 +1,55 @@
+package org.ck.adventofcode.year2021.day03;
+
+import org.ck.codeChallengeLib.annotation.Solution;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+@Solution(
+    id = 20210302,
+    name = "Day 3: Binary Diagnostic - Part 2",
+    url = "https://adventofcode.com/2021/day/3#part2",
+    category = "2021")
+public class Part2 {
+  public static void main(String[] args) {
+    List<String> numbers = new ArrayList<>();
+
+    try (Scanner in = new Scanner(System.in)) {
+      while (in.hasNextLine()) {
+        numbers.add(in.nextLine());
+      }
+    }
+
+    int generator = getValue(numbers, (ones, size) -> 2 * ones >= size);
+    int scrubbers = getValue(numbers, (ones, size) -> 2 * ones < size);
+
+    System.out.println(generator * scrubbers);
+  }
+
+  private static int getValue(List<String> numbers, BiPredicate<Integer, Integer> predicate) {
+    List<String> values = new ArrayList<>(numbers);
+
+    int length = values.get(0).length();
+
+    for (int i = 0; i < length; ++i) {
+      int finalI = i;
+      int ones = values.stream().mapToInt(line -> line.charAt(finalI) - '0').sum();
+
+      Predicate<String> pick =
+          predicate.test(ones, values.size())
+              ? line -> line.charAt(finalI) == '1'
+              : line -> line.charAt(finalI) != '1';
+
+      values = values.stream().filter(pick).toList();
+
+      if (values.size() == 1) {
+        break;
+      }
+    }
+
+    return Integer.parseInt(values.get(0), 2);
+  }
+}

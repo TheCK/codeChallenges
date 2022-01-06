28bf2900eab24b352fd5d272bf0c9efee9a1b442
TheCK <karsten_knuth@gmx.net>
2022-01-06 16:26:54 +0100

    2021/19/1

--- adventofcode/src/main/java/org/ck/adventofcode/year2021/day19/Part1.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2021/day19/Part1.java
@@ -3,9 +3,10 @@ package org.ck.adventofcode.year2021.day19;
 import org.ck.codeChallengeLib.annotation.Solution;
 
 import java.util.*;
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Solution(
     id = 20211901,
@@ -16,68 +17,144 @@ import java.util.regex.Pattern;
 public class Part1 {
   private static final Pattern scannerPattern = Pattern.compile("--- scanner (\\d+) ---");
 
+  private static final List<UnaryOperator<Position>> rotators = List.of(
+          position -> new Position(position.x(), position.y(), position.z()),
+          position -> new Position(position.y(), -position.x(), position.z()),
+          position -> new Position(-position.x(), -position.y(), position.z()),
+          position -> new Position(-position.y(), position.x(), position.z()),
+
+          position -> new Position(position.y(), position.z(), position.x()),
+          position -> new Position(-position.x(), position.z(), position.y()),
+          position -> new Position(-position.y(), position.z(), -position.x()),
+          position -> new Position(position.x(), position.z(), -position.y()),
+
+          position -> new Position(position.z(), position.x(), position.y()),
+          position -> new Position(position.z(), position.y(), -position.x()),
+          position -> new Position(position.z(), -position.x(), -position.y()),
+          position -> new Position(position.z(), -position.y(), position.x()),
+
+          position -> new Position(position.y(), position.x(), -position.z()),
+          position -> new Position(-position.x(), position.y(), -position.z()),
+          position -> new Position(-position.y(), -position.x(), -position.z()),
+          position -> new Position(position.x(), -position.y(), -position.z()),
+
+          position -> new Position(position.x(), -position.z(), position.y()),
+          position -> new Position(position.y(), -position.z(), -position.x()),
+          position -> new Position(-position.x(), -position.z(), -position.y()),
+          position -> new Position(-position.y(), -position.z(), position.x()),
+
+          position -> new Position(-position.z(), position.y(), position.x()),
+          position -> new Position(-position.z(), -position.x(), position.y()),
+          position -> new Position(-position.z(), -position.y(), -position.x()),
+          position -> new Position(-position.z(), position.x(), -position.y())
+  );
+
   public static void main(String[] args) {
-    Map<Integer, Set<Beacon>> beacons = new HashMap<>();
+    Set<Sensor> sensors = new HashSet<>();
 
     try (Scanner in = new Scanner(System.in)) {
-      int scanner = 0;
+      int sensorName = 0;
+      Set<Position> beacons = new HashSet<>();
 
       while (in.hasNextLine()) {
         String line = in.nextLine();
 
         final Matcher matcher = scannerPattern.matcher(line);
         if (matcher.find()) {
-          scanner = Integer.parseInt(matcher.group(1));
-          beacons.put(scanner, new HashSet<>());
+          sensorName = Integer.parseInt(matcher.group(1));
+          beacons = new HashSet<>();
         } else if (!line.isBlank()) {
           final String[] split = line.split(",");
-          beacons
-              .get(scanner)
-              .add(
-                  new Beacon(
-                      Integer.parseInt(split[0]),
-                      Integer.parseInt(split[1]),
-                      Integer.parseInt(split[2])));
+          beacons.add(
+              new Position(
+                  Integer.parseInt(split[0]),
+                  Integer.parseInt(split[1]),
+                  Integer.parseInt(split[2])));
+        } else {
+          sensors.add(new Sensor(sensorName, new Position(0, 0, 0), beacons));
         }
       }
+
+      sensors.add(new Sensor(sensorName, new Position(0, 0, 0), beacons));
     }
 
-    for (int i = 0; i < beacons.size(); ++i) {
-      for (int j = i + 1; j < beacons.size(); ++j) {
-        for (Beacon beacon1 : beacons.get(i)) {
-          for (Beacon beacon2 : beacons.get(j)) {
-            Function<Beacon, Set<Beacon>> aliases = getAliases(beacon1, beacon2);
-            int same = 0;
+    List<Sensor> uncorrected = new ArrayList<>(sensors);
 
-            for (Beacon candidate : beacons.get(j)) {
-              final int finalI = i;
-              if (aliases.apply(candidate).stream()
-                  .anyMatch(beacon -> beacons.get(finalI).contains(beacon))) {
-                ++same;
-              }
-            }
+    Queue<Sensor> alreadyCorrected = new ArrayDeque<>();
 
-            if (same >= 12) {
-              System.err.println(i + " next to " + j);
+    sensors.stream().filter(sensor -> sensor.number == 0).findFirst().ifPresent(first -> {
+      alreadyCorrected.add(first);
+      uncorrected.remove(first);
+    });
+
+    while (!alreadyCorrected.isEmpty()) {
+      Sensor corrected = alreadyCorrected.remove();
+
+      Set<Sensor> moved = new HashSet<>();
+      for (Sensor other : uncorrected) {
+        corrected
+            .correctPosition(other)
+            .ifPresent(
+                fixed -> {
+                  alreadyCorrected.add(fixed);
+                  sensors.remove(fixed);
+                  sensors.add(fixed);
+                  moved.add(other);
+                });
+      }
+      uncorrected.removeAll(moved);
+    }
+
+    System.out.println(
+        sensors.stream().flatMap(sensor -> sensor.beacons().stream()).distinct().count());
+  }
+
+  private record Position(int x, int y, int z) {}
+
+  private record Sensor(int number, Position position, Set<Position> beacons) {
+    private Optional<Sensor> correctPosition(Sensor other) {
+      for(UnaryOperator<Position> rotator : rotators) {
+        Sensor rotated = other.rotate(rotator);
+        for(Position beacon : beacons) {
+          for(Position otherBeacon : rotated.beacons) {
+            Position offset = new Position(beacon.x - otherBeacon.x, beacon.y - otherBeacon.y, beacon.z - otherBeacon.z);
+            Sensor candidate = rotated.move(offset);
+            if(enoughBeaconsMatch(candidate)) {
+              return Optional.of(candidate);
             }
           }
         }
       }
+
+      return Optional.empty();
     }
 
-    System.out.println("foo");
-  }
+    private Sensor move(final Position offset) {
+      return new Sensor(number, offset, beacons.stream().map(beacon -> new Position(beacon.x() + offset.x(), beacon.y() + offset.y(), beacon.z() + offset.z())).collect(Collectors.toSet()));
+    }
 
-  private static Function<Beacon, Set<Beacon>> getAliases(
-      final Beacon beacon1, final Beacon beacon2) {
-    return (beacon ->
-        new HashSet<>(
-            List.of(
-                new Beacon(beacon.x - Math.abs(beacon2.x - beacon1.x), 0, 0),
-                new Beacon(beacon.x - Math.abs(-beacon2.x - beacon1.x), 0, 0),
-                new Beacon(beacon.x - Math.abs(beacon2.x + beacon1.x), 0, 0),
-                new Beacon(beacon.x - Math.abs(-beacon2.x + beacon1.x), 0, 0))));
-  }
+    private Sensor rotate(UnaryOperator<Position> rotator) {
+      return new Sensor(number, rotator.apply(position), beacons.stream().map(rotator).collect(Collectors.toSet()));
+    }
 
-  private static final record Beacon(int x, int y, int z) {}
+    private boolean enoughBeaconsMatch(final Sensor other) {
+      Set<Position> beacons = new HashSet<>(this.beacons);
+      beacons.retainAll(other.beacons);
+
+      return beacons.size() >= 12;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      final Sensor sensor = (Sensor) o;
+      return number == sensor.number;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(number);
+    }
+  }
 }
--- adventofcode/src/test/java/org/ck/adventofcode/year2021/day19/Part1Test.java
+++ adventofcode/src/test/java/org/ck/adventofcode/year2021/day19/Part1Test.java
@@ -1,12 +1,10 @@
 package org.ck.adventofcode.year2021.day19;
 
 import org.ck.codeChallengeLib.testhelper.BaseTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class Part1Test extends BaseTest {
-  @Disabled
   @ParameterizedTest
   @ValueSource(strings = {"01", "01a"})
   public void test(String name) throws Exception {

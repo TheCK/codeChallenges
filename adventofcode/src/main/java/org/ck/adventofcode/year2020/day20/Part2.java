c4b3476821e42ed6e78ec304ed6db42faa4a5814
TheCK <karsten_knuth@gmx.net>
2022-01-31 18:42:07 +0100

    finished aoc 2020

--- adventofcode/src/main/java/org/ck/adventofcode/year2020/day20/Part2.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2020/day20/Part2.java
@@ -12,8 +12,7 @@ import java.util.regex.Pattern;
     id = 20202002,
     name = "Day 20: Jurassic Jigsaw - Part 2",
     url = "https://adventofcode.com/2020/day/20",
-    category = "2020",
-    solved = false)
+    category = "2020")
 public class Part2 {
   private static final Pattern tileNamePattern = Pattern.compile("Tile ([0-9]+):");
 
@@ -50,6 +49,72 @@ public class Part2 {
     }
 
     Tile topLeft = tiles.stream().filter(tile -> tile.getNeighbours().size() == 2).findAny().get();
+
+    while (topLeft.getNeighbour(Tile.Edge.TOP) != null
+        || topLeft.getNeighbour(Tile.Edge.LEFT) != null) {
+      topLeft.rotateCW();
+    }
+
+    Tile first = topLeft;
+    Tile tile = topLeft;
+
+    List<List<Character>> bigMap = new ArrayList<>();
+
+    int row = 0;
+
+    while (tile != null) {
+      List<char[]> pixels = tile.getPixels();
+
+      for (int y = 0; y < pixels.size() - 2; ++y) {
+        char[] currentRow = pixels.get(y);
+        while (bigMap.size() <= (row * (pixels.size() - 2)) + y) {
+          bigMap.add(new ArrayList<>());
+        }
+
+        for (int x = 0; x < currentRow.length - 2; ++x) {
+          bigMap.get((row * (pixels.size() - 2)) + y).add(pixels.get(y + 1)[x + 1]);
+        }
+      }
+
+      Tile right = tile.getNeighbour(Tile.Edge.RIGHT);
+      if (right != null) {
+        tile = right;
+      } else {
+        ++row;
+
+        tile = first.getNeighbour(Tile.Edge.BOTTOM);
+        first = tile;
+      }
+    }
+
+    Tile bigTile = new Tile(0);
+
+    for (List<Character> currentRow : bigMap) {
+      char[] newRow = new char[currentRow.size()];
+
+      for (int i = 0; i < currentRow.size(); ++i) {
+        newRow[i] = currentRow.get(i);
+      }
+
+      bigTile.addRow(newRow);
+    }
+
+    int state = 0;
+
+    while (state < 8) {
+      switch (state) {
+        case 1, 2, 3, 5, 6, 7 -> bigTile.rotateCW();
+        case 4 -> bigTile.flipX();
+      }
+
+      int monsters = bigTile.countMonsters();
+      if (monsters > 0) {
+        System.out.println(bigTile.countRoughness(monsters));
+        break;
+      }
+
+      ++state;
+    }
   }
 
   private static class Tile {
@@ -58,49 +123,136 @@ public class Part2 {
     private static final Map<String, Consumer<Tile>> actions =
         new HashMap<>() {
           {
-            /*
-            TOP,
-            BOTTOM,
-            LEFT,
-            RIGHT,
-            TOP_REVERSE,
-            BOTTOM_REVERSE,
-            LEFT_REVERSE,
-            RIGHT_REVERSE;
-                   */
-            put(String.format(format, Edge.TOP, Edge.TOP), Tile::flipX);
-            put(String.format(format, Edge.TOP, Edge.TOP), Tile::flipBoth);
+            put(String.format(format, Edge.TOP, Edge.TOP), Tile::nothing);
+            put(String.format(format, Edge.TOP, Edge.TOP_REVERSE), Tile::nothing);
             put(String.format(format, Edge.TOP, Edge.BOTTOM), Tile::nothing);
-            put(String.format(format, Edge.TOP, Edge.BOTTOM_REVERSE), Tile::flipY);
-
-            // TODO
-            put(String.format(format, Edge.TOP, Edge.LEFT), null /* Tile::rotateCC */);
-            put(String.format(format, Edge.TOP, Edge.LEFT_REVERSE), Tile::flipBoth);
+            put(String.format(format, Edge.TOP, Edge.BOTTOM_REVERSE), Tile::nothing);
+            put(String.format(format, Edge.TOP, Edge.LEFT), Tile::nothing);
+            put(String.format(format, Edge.TOP, Edge.LEFT_REVERSE), Tile::nothing);
             put(String.format(format, Edge.TOP, Edge.RIGHT), Tile::nothing);
-            put(String.format(format, Edge.TOP, Edge.RIGHT_REVERSE), Tile::flipY);
+            put(String.format(format, Edge.TOP, Edge.RIGHT_REVERSE), Tile::nothing);
+
+            put(String.format(format, Edge.LEFT, Edge.TOP), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.TOP_REVERSE), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.BOTTOM), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.BOTTOM_REVERSE), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.LEFT), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.LEFT_REVERSE), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.RIGHT), Tile::nothing);
+            put(String.format(format, Edge.LEFT, Edge.RIGHT_REVERSE), Tile::nothing);
+
+            put(
+                String.format(format, Edge.RIGHT, Edge.TOP),
+                tile -> {
+                  tile.rotateCCW();
+                  tile.flipX();
+                });
+            put(String.format(format, Edge.RIGHT, Edge.TOP_REVERSE), Tile::rotateCCW);
+            put(String.format(format, Edge.RIGHT, Edge.BOTTOM), Tile::rotateCW);
+            put(
+                String.format(format, Edge.RIGHT, Edge.BOTTOM_REVERSE),
+                tile -> {
+                  tile.rotateCW();
+                  tile.flipX();
+                });
+
+            put(String.format(format, Edge.RIGHT, Edge.LEFT), Tile::nothing);
+            put(String.format(format, Edge.RIGHT, Edge.LEFT_REVERSE), Tile::flipX);
+            put(String.format(format, Edge.RIGHT, Edge.RIGHT), Tile::flipY);
+            put(String.format(format, Edge.RIGHT, Edge.RIGHT_REVERSE), Tile::flipBoth);
+
+            put(String.format(format, Edge.BOTTOM, Edge.TOP), Tile::nothing);
+            put(String.format(format, Edge.BOTTOM, Edge.TOP_REVERSE), Tile::flipY);
+            put(String.format(format, Edge.BOTTOM, Edge.BOTTOM), Tile::flipX);
+            put(String.format(format, Edge.BOTTOM, Edge.BOTTOM_REVERSE), Tile::flipBoth);
+
+            put(
+                String.format(format, Edge.BOTTOM, Edge.LEFT),
+                tile -> {
+                  tile.rotateCW();
+                  tile.flipY();
+                });
+            put(String.format(format, Edge.BOTTOM, Edge.LEFT_REVERSE), Tile::rotateCW);
+            put(String.format(format, Edge.BOTTOM, Edge.RIGHT), Tile::rotateCCW);
+            put(
+                String.format(format, Edge.BOTTOM, Edge.RIGHT_REVERSE),
+                tile -> {
+                  tile.rotateCCW();
+                  tile.flipY();
+                });
           }
         };
-
-    private static void nothing(Tile tile) {
-      // nothing
-    }
-
-    private static void flipBoth(Tile tile) {
-      flipY(tile);
-      flipX(tile);
-    }
-
-    private static void flipX(Tile tile) {}
-
-    private static void flipY(Tile tile) {}
-
     private long id;
     private List<char[]> pixels = new ArrayList<>();
-
     private List<Tile> neighbours = new ArrayList<>();
 
     public Tile(long id) {
       this.id = id;
+    }
+
+    public List<char[]> getPixels() {
+      return pixels;
+    }
+
+    private void nothing() {}
+
+    private void flipBoth() {
+      flipY();
+      flipX();
+    }
+
+    private void flipX() {
+      List<char[]> newPixels = new ArrayList<>();
+
+      for (int i = 0; i < pixels.size(); ++i) {
+        newPixels.add(new char[pixels.get(0).length]);
+      }
+
+      for (int x = 0; x < pixels.size(); ++x) {
+        for (int y = 0; y < pixels.get(x).length; ++y) {
+          newPixels.get(pixels.size() - y - 1)[x] = pixels.get(y)[x];
+        }
+      }
+
+      pixels = newPixels;
+    }
+
+    private void flipY() {
+      List<char[]> newPixels = new ArrayList<>();
+
+      for (int i = 0; i < pixels.size(); ++i) {
+        newPixels.add(new char[pixels.get(0).length]);
+      }
+
+      for (int x = 0; x < pixels.size(); ++x) {
+        for (int y = 0; y < pixels.get(x).length; ++y) {
+          newPixels.get(y)[pixels.get(y).length - x - 1] = pixels.get(y)[x];
+        }
+      }
+
+      pixels = newPixels;
+    }
+
+    private void rotateCCW() {
+      rotateCW();
+      rotateCW();
+      rotateCW();
+    }
+
+    public void rotateCW() {
+      List<char[]> newPixels = new ArrayList<>();
+
+      for (int i = 0; i < pixels.get(0).length; ++i) {
+        newPixels.add(new char[pixels.size()]);
+      }
+
+      for (int x = 0; x < pixels.size(); ++x) {
+        for (int y = pixels.get(x).length - 1; y >= 0; --y) {
+          newPixels.get(x)[newPixels.size() - y - 1] = pixels.get(y)[x];
+        }
+      }
+
+      pixels = newPixels;
     }
 
     public long getId() {
@@ -151,7 +303,7 @@ public class Part2 {
     }
 
     public void arrange(Edge other, Edge own) {
-      actions.get(String.format(format, other, own));
+      actions.get(String.format(format, other, own)).accept(this);
     }
 
     private char[] getEdge(Edge edge) {
@@ -195,6 +347,48 @@ public class Part2 {
       }
 
       return null;
+    }
+
+    public int countMonsters() {
+      int monsters = 0;
+
+      for (int y = 0; y < pixels.size() - 2; ++y) {
+        for (int x = 0; x < pixels.get(y).length - 19; ++x) {
+          if (pixels.get(y)[x + 18] == '#'
+              && pixels.get(y + 1)[x] == '#'
+              && pixels.get(y + 1)[x + 5] == '#'
+              && pixels.get(y + 1)[x + 6] == '#'
+              && pixels.get(y + 1)[x + 11] == '#'
+              && pixels.get(y + 1)[x + 12] == '#'
+              && pixels.get(y + 1)[x + 17] == '#'
+              && pixels.get(y + 1)[x + 18] == '#'
+              && pixels.get(y + 1)[x + 19] == '#'
+              && pixels.get(y + 2)[x + 1] == '#'
+              && pixels.get(y + 2)[x + 4] == '#'
+              && pixels.get(y + 2)[x + 7] == '#'
+              && pixels.get(y + 2)[x + 10] == '#'
+              && pixels.get(y + 2)[x + 13] == '#'
+              && pixels.get(y + 2)[x + 16] == '#') {
+            ++monsters;
+          }
+        }
+      }
+
+      return monsters;
+    }
+
+    public int countRoughness(final int monsters) {
+      int roughness = 0;
+
+      for (int y = 0; y < pixels.size(); ++y) {
+        for (int x = 0; x < pixels.get(y).length; ++x) {
+          if (pixels.get(y)[x] == '#') {
+            ++roughness;
+          }
+        }
+      }
+
+      return roughness - monsters * 15;
     }
 
     private enum Edge {

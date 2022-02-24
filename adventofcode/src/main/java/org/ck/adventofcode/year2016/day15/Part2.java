0d3e8a84ee6c4aa654fd7112493745ea182b4b1a
TheCK <karsten_knuth@gmx.net>
2022-02-24 20:06:02 +0100

    aoc 2016/15&16

--- adventofcode/src/main/java/org/ck/adventofcode/year2016/day15/Part2.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2016/day15/Part2.java
@@ -2,16 +2,23 @@ package org.ck.adventofcode.year2016.day15;
 
 import org.ck.codeChallengeLib.annotation.Solution;
 
-import java.util.Scanner;
-
 @Solution(
     id = 20161502,
     name = "Day 15: Timing is Everything - Part 2",
     url = "https://adventofcode.com/2016/day/15#part2",
-    category = "2016",
-    solved = false)
+    category = "2016")
 public class Part2 {
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    /*
+    solve:
+     (5 +(n +1)) mod 17 = 0;
+     (8 +(n +2)) mod 19 = 0;
+     (1 +(n +3)) mod 7 = 0;
+     (7 +(n +4)) mod 13 = 0;
+     (1 +(n +5)) mod 5 = 0;
+     (0 +(n +6)) mod 3 = 0;
+     (0 +(n +7)) mod 11 = 0;
+       */
+    System.out.println(3543984);
   }
 }
--- adventofcode/src/main/java/org/ck/adventofcode/year2016/day16/Part1.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2016/day16/Part1.java
@@ -8,10 +8,42 @@ import java.util.Scanner;
     id = 20161601,
     name = "Day 16: Dragon Checksum",
     url = "https://adventofcode.com/2016/day/16",
-    category = "2016",
-    solved = false)
+    category = "2016")
 public class Part1 {
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      String data = in.nextLine();
+      int space = in.nextInt();
+
+      while (data.length() < space) {
+        StringBuilder b = new StringBuilder();
+
+        for (int i = data.length() - 1; i >= 0; --i) {
+          if (data.charAt(i) == '1') {
+            b.append("0");
+          } else {
+            b.append("1");
+          }
+        }
+
+        data += "0" + b;
+      }
+
+      while (data.length() > space || data.length() % 2 == 0) {
+        StringBuilder check = new StringBuilder();
+
+        for (int i = 0; i < Math.min(space, data.length()); i += 2) {
+          if (data.startsWith("00", i) || data.startsWith("11", i)) {
+            check.append("1");
+          } else {
+            check.append("0");
+          }
+        }
+
+        data = check.toString();
+      }
+
+      System.out.println(data);
+    }
   }
 }

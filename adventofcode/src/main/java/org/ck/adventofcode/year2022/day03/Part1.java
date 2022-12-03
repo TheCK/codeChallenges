6f4fc5da916a74a18171d12bd29eddf6015e984f
TheCK <karsten_knuth@gmx.net>
2022-12-03 14:49:47 +0100

    AOC 22 day 3

--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day03/Part1.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day03/Part1.java
@@ -3,15 +3,39 @@ package org.ck.adventofcode.year2022.day03;
 import org.ck.codeChallengeLib.annotation.Solution;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Solution(
     id = 20220301,
     name = "Day 3: ???",
     url = "https://adventofcode.com/2022/day/3",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part1 {
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      int priority = 0;
+
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+
+        String compartment1 = line.substring(0, line.length() / 2);
+        String compartment2 = line.substring(line.length() / 2);
+
+        final Set<Integer> items1 = compartment1.chars().boxed().collect(Collectors.toSet());
+        final Set<Integer> items2 = compartment2.chars().boxed().collect(Collectors.toSet());
+
+        items1.retainAll(items2);
+
+        int item = items1.stream().findFirst().get();
+        if (Character.isUpperCase(item)) {
+          priority += item - 'A' + 27;
+        } else {
+          priority += item - 'a' + 1;
+        }
+      }
+
+      System.out.println(priority);
+    }
   }
 }
--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day03/Part2.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day03/Part2.java
@@ -3,15 +3,40 @@ package org.ck.adventofcode.year2022.day03;
 import org.ck.codeChallengeLib.annotation.Solution;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Solution(
     id = 20220302,
     name = "Day 3: ??? - Part 2",
     url = "https://adventofcode.com/2022/day/3#part2",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part2 {
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      int priority = 0;
+
+      while (in.hasNextLine()) {
+        String line1 = in.nextLine();
+        String line2 = in.nextLine();
+        String line3 = in.nextLine();
+
+        final Set<Integer> items1 = line1.chars().boxed().collect(Collectors.toSet());
+        final Set<Integer> items2 = line2.chars().boxed().collect(Collectors.toSet());
+        final Set<Integer> items3 = line3.chars().boxed().collect(Collectors.toSet());
+
+        items1.retainAll(items2);
+        items1.retainAll(items3);
+
+        int item = items1.stream().findFirst().get();
+        if (Character.isUpperCase(item)) {
+          priority += item - 'A' + 27;
+        } else {
+          priority += item - 'a' + 1;
+        }
+      }
+
+      System.out.println(priority);
+    }
   }
 }

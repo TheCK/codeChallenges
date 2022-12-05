afc752d68562ba9c8f490f53550dd7d07ee7ce01
TheCK <karsten_knuth@gmx.net>
2022-12-05 10:21:21 +0100

    AOC 22 day 5

--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day05/Part1.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day05/Part1.java
@@ -2,16 +2,66 @@ package org.ck.adventofcode.year2022.day05;
 
 import org.ck.codeChallengeLib.annotation.Solution;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Solution(
     id = 20220501,
-    name = "Day 5: ???",
+    name = "Day 5: Supply Stacks",
     url = "https://adventofcode.com/2022/day/5",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part1 {
+  private static final Pattern COMMAND_PATTERN =
+      Pattern.compile("move (\\d+) from (\\d+) to (\\d+)");
+
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      List<String> inputsLines = new ArrayList<>();
+
+      String line = in.nextLine();
+      while (!line.isEmpty()) {
+        inputsLines.add(line);
+
+        line = in.nextLine();
+      }
+
+      int numberOfStacks = inputsLines.get(inputsLines.size() - 1).trim().split("\\s+").length;
+
+      List<Deque<Character>> stacks = new ArrayList<>();
+      for (int i = 0; i < numberOfStacks; ++i) {
+        stacks.add(new ArrayDeque<>());
+      }
+
+      for (int i = inputsLines.size() - 2; i >= 0; --i) {
+        for (int stack = 0; stack < numberOfStacks; ++stack) {
+          if ((stack * 4 + 1) < inputsLines.get(i).length()
+              && inputsLines.get(i).charAt(stack * 4 + 1) != ' ') {
+            stacks.get(stack).push(inputsLines.get(i).charAt(stack * 4 + 1));
+          }
+        }
+      }
+
+      while (in.hasNextLine()) {
+        String command = in.nextLine();
+        Matcher matcher = COMMAND_PATTERN.matcher(command);
+
+        if (matcher.find()) {
+          int count = Integer.parseInt(matcher.group(1));
+          int from = Integer.parseInt(matcher.group(2)) - 1;
+          int to = Integer.parseInt(matcher.group(3)) - 1;
+
+          for (int i = 0; i < count; ++i) {
+            stacks.get(to).push(stacks.get(from).pop());
+          }
+        }
+      }
+
+      StringBuilder builder = new StringBuilder();
+      for (Deque<Character> stack : stacks) {
+        builder.append(stack.pop());
+      }
+      System.out.println(builder);
+    }
   }
 }
--- adventofcode/src/test/java/org/ck/adventofcode/year2022/day05/Part1Test.java
+++ adventofcode/src/test/java/org/ck/adventofcode/year2022/day05/Part1Test.java
@@ -1,11 +1,9 @@
 package org.ck.adventofcode.year2022.day05;
 
 import org.ck.codeChallengeLib.testhelper.BaseTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@Disabled
 public class Part1Test extends BaseTest {
   @ParameterizedTest
   @ValueSource(strings = {"01", "01a"})

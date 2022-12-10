5fde8414a62d9aac954ff9f2ef1427db50733980
TheCK <karsten_knuth@gmx.net>
2022-12-10 15:43:59 +0100

    AOC 22 day 10

--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day10/Part1.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day10/Part1.java
@@ -2,16 +2,43 @@ package org.ck.adventofcode.year2022.day10;
 
 import org.ck.codeChallengeLib.annotation.Solution;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 @Solution(
     id = 20221001,
-    name = "Day 10: ???",
+    name = "Day 10: Cathode-Ray Tube",
     url = "https://adventofcode.com/2022/day/10",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part1 {
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      int acc = 1;
+      List<Integer> accHistory = new ArrayList<>();
+
+      while (in.hasNextLine()) {
+        String[] command = in.nextLine().split(" ");
+
+        switch (command[0]) {
+          case "noop":
+            accHistory.add(acc);
+            break;
+          case "addx":
+            accHistory.add(acc);
+            accHistory.add(acc);
+            acc = acc + Integer.parseInt(command[1]);
+            break;
+        }
+      }
+
+      int signalStrength = 0;
+
+      for (int i = 19; i < accHistory.size(); i += 40) {
+        signalStrength += (i + 1) * accHistory.get(i);
+      }
+
+      System.out.println(signalStrength);
+    }
   }
 }
--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day10/Part2.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day10/Part2.java
@@ -2,16 +2,50 @@ package org.ck.adventofcode.year2022.day10;
 
 import org.ck.codeChallengeLib.annotation.Solution;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 @Solution(
     id = 20221002,
-    name = "Day 10: ??? - Part 2",
+    name = "Day 10: Cathode-Ray Tube - Part 2",
     url = "https://adventofcode.com/2022/day/10#part2",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part2 {
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      int acc = 1;
+      List<Integer> accHistory = new ArrayList<>();
+
+      while (in.hasNextLine()) {
+        String[] command = in.nextLine().split(" ");
+
+        switch (command[0]) {
+          case "noop":
+            accHistory.add(acc);
+            break;
+          case "addx":
+            accHistory.add(acc);
+            accHistory.add(acc);
+            acc = acc + Integer.parseInt(command[1]);
+            break;
+        }
+      }
+
+      StringBuilder screen = new StringBuilder();
+      for (int i = 1; i <= 240; ++i) {
+        if (Math.abs(accHistory.get(i - 1) - ((i - 1) % 40)) <= 1) {
+          screen.append('#');
+        } else {
+          screen.append('.');
+        }
+
+        if (i % 40 == 0 && i != 240) {
+          screen.append(System.lineSeparator());
+        }
+      }
+
+      System.out.println(screen);
+    }
   }
 }

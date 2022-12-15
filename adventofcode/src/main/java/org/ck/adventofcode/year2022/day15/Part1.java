203c1bee237f8826e3a75e76e673c95e30d103cd
TheCK <karsten_knuth@gmx.net>
2022-12-15 17:46:59 +0100

    AOC 22 day 15

--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day15/Part1.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day15/Part1.java
@@ -2,16 +2,61 @@ package org.ck.adventofcode.year2022.day15;
 
 import org.ck.codeChallengeLib.annotation.Solution;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Solution(
     id = 20221501,
-    name = "Day 15: ???",
+    name = "Day 15: Beacon Exclusion Zone",
     url = "https://adventofcode.com/2022/day/15",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part1 {
+  private static final Pattern PATTERN =
+      Pattern.compile(
+          "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      int row = in.nextInt();
+      List<Sensor> sensors = new ArrayList<>();
+      Set<Point> beacons = new HashSet<>();
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+
+        Matcher matcher = PATTERN.matcher(line);
+        if (matcher.matches()) {
+          int sensorX = Integer.parseInt(matcher.group(1));
+          int sensorY = Integer.parseInt(matcher.group(2));
+
+          int beaconX = Integer.parseInt(matcher.group(3));
+          int beaconY = Integer.parseInt(matcher.group(4));
+
+          int distance = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
+          sensors.add(new Sensor(sensorX, sensorY, distance));
+          beacons.add(new Point(beaconX, beaconY));
+        }
+      }
+
+      Set<Point> reachableInRow = new HashSet<>();
+      for (Sensor sensor : sensors) {
+        int xDistance = sensor.range() - Math.abs(row - sensor.y());
+
+        if (xDistance >= 0) {
+          for (int x = sensor.x() - xDistance; x <= sensor.x() + xDistance; ++x) {
+            Point point = new Point(x, row);
+            if (!beacons.contains(point)) {
+              reachableInRow.add(point);
+            }
+          }
+        }
+      }
+
+      System.out.println(reachableInRow.size());
+    }
   }
+
+  record Sensor(int x, int y, int range) {}
+
+  record Point(int x, int y) {}
 }
--- adventofcode/src/main/java/org/ck/adventofcode/year2022/day15/Part2.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2022/day15/Part2.java
@@ -2,16 +2,70 @@ package org.ck.adventofcode.year2022.day15;
 
 import org.ck.codeChallengeLib.annotation.Solution;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Solution(
     id = 20221502,
-    name = "Day 15: ??? - Part 2",
+    name = "Day 15: Beacon Exclusion Zone - Part 2",
     url = "https://adventofcode.com/2022/day/15#part2",
-    category = "2022",
-    solved = false)
+    category = "2022")
 public class Part2 {
+  private static final Pattern PATTERN =
+      Pattern.compile(
+          "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+
   public static void main(String[] args) {
-    try (Scanner in = new Scanner(System.in)) {}
+    try (Scanner in = new Scanner(System.in)) {
+      long max = in.nextLong();
+      List<Sensor> sensors = new ArrayList<>();
+      while (in.hasNextLine()) {
+        String line = in.nextLine();
+
+        Matcher matcher = PATTERN.matcher(line);
+        if (matcher.matches()) {
+          long sensorX = Long.parseLong(matcher.group(1));
+          long sensorY = Long.parseLong(matcher.group(2));
+
+          long beaconX = Long.parseLong(matcher.group(3));
+          long beaconY = Long.parseLong(matcher.group(4));
+
+          long distance = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
+          sensors.add(new Sensor(sensorX, sensorY, distance));
+        }
+      }
+
+      for (Sensor sensor : sensors) {
+        long minX = sensor.x() - sensor.range() - 1;
+
+        for (long x = minX, y = sensor.y(); x <= sensor.x(); ++x, --y) {
+          if (x < 0 || y < 0 || x > max || y > max) {
+            continue;
+          }
+
+          if (checkAllSensors(x, y, sensors)) {
+            System.out.println(x * 4000000 + y);
+            return;
+          }
+        }
+      }
+    }
   }
+
+  private static boolean checkAllSensors(final long x, final long y, final List<Sensor> sensors) {
+    for (Sensor sensor : sensors) {
+      long distance = Math.abs(sensor.x() - x) + Math.abs(sensor.y() - y);
+
+      if (sensor.range() >= distance) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  record Sensor(long x, long y, long range) {}
 }

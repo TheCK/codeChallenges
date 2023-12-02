702966e6fd2830ccfffaae50eeea854ca5fc1543
TheCK <karsten_knuth@gmx.net>
2023-12-02 20:13:40 +0100

    AOC 2023 day 2 refactoring

--- adventofcode/src/main/java/org/ck/adventofcode/year2023/Day02.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2023/Day02.java
@@ -1,7 +1,6 @@
 package org.ck.adventofcode.year2023;
 
 import java.util.Scanner;
-import java.util.function.ToLongFunction;
 import org.ck.adventofcode.util.AOCSolution;
 import org.ck.codechallengelib.annotation.Solution;
 
@@ -20,33 +19,9 @@ public class Day02 extends AOCSolution {
   protected void runPartOne(final Scanner in) {
     run(
         in,
-        game -> {
-          boolean valid = true;
-          for (final String round : game[1].split("; ")) {
-            for (final String draw : round.split(", ")) {
-              final String[] colorAmount = draw.split(" ");
-
-              valid =
-                  Integer.parseInt(colorAmount[0])
-                      <= switch (colorAmount[1]) {
-                        case "red" -> 12;
-                        case "green" -> 13;
-                        case "blue" -> 14;
-                        default -> 0;
-                      };
-
-              if (!valid) {
-                break;
-              }
-            }
-
-            if (!valid) {
-              break;
-            }
-          }
-
-          if (valid) {
-            return Integer.parseInt(game[0].substring(5));
+        (game, reds, greens, blues) -> {
+          if (reds <= 12 && greens <= 13 && blues <= 14) {
+            return Integer.parseInt(game.substring(5));
           }
 
           return 0;
@@ -55,41 +30,41 @@ public class Day02 extends AOCSolution {
 
   @Override
   protected void runPartTwo(final Scanner in) {
-    run(
-        in,
-        game -> {
-          long reds = 0;
-          long greens = 0;
-          long blues = 0;
-
-          for (final String round : game[1].split("; ")) {
-            for (final String draw : round.split(", ")) {
-              final String[] colorAmount = draw.split(" ");
-
-              final long amount = Long.parseLong(colorAmount[0]);
-
-              switch (colorAmount[1]) {
-                case "red" -> reds = Math.max(reds, amount);
-                case "green" -> greens = Math.max(greens, amount);
-                case "blue" -> blues = Math.max(blues, amount);
-                default -> {}
-              }
-            }
-          }
-
-          return reds * greens * blues;
-        });
+    run(in, (game, reds, greens, blues) -> reds * greens * blues);
   }
 
-  private void run(final Scanner in, final ToLongFunction<String[]> getGameValue) {
+  private void run(final Scanner in, final GetGameValueFunction getGameValue) {
     long sum = 0;
 
     while (in.hasNextLine()) {
       final String[] game = in.nextLine().split(": ");
 
-      sum += getGameValue.applyAsLong(game);
+      long reds = 0;
+      long greens = 0;
+      long blues = 0;
+
+      for (final String round : game[1].split("; ")) {
+        for (final String draw : round.split(", ")) {
+          final String[] colorAmount = draw.split(" ");
+
+          final long amount = Long.parseLong(colorAmount[0]);
+
+          switch (colorAmount[1]) {
+            case "red" -> reds = Math.max(reds, amount);
+            case "green" -> greens = Math.max(greens, amount);
+            case "blue" -> blues = Math.max(blues, amount);
+            default -> throw new IllegalStateException("");
+          }
+        }
+      }
+
+      sum += getGameValue.get(game[0], reds, greens, blues);
     }
 
     print(sum);
+  }
+
+  private interface GetGameValueFunction {
+    long get(String game, long reds, long greens, long blues);
   }
 }

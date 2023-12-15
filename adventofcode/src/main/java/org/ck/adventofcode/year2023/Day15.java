52d516007d8c93750e6d8db6c3252bbb59b42888
TheCK <karsten_knuth@gmx.net>
2023-12-15 10:09:52 +0100

    AOC 2023 day 15

--- adventofcode/src/main/java/org/ck/adventofcode/year2023/Day15.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2023/Day15.java
@@ -1,31 +1,111 @@
 package org.ck.adventofcode.year2023;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.BiFunction;
 import org.ck.adventofcode.util.AOCSolution;
 import org.ck.codechallengelib.annotation.Solution;
 
 @Solution(
     id = 20231501,
-    name = "Day 15: ",
+    name = "Day 15: Lens Library",
     url = "https://adventofcode.com/2023/day/15",
-    category = "2023",
-    solved = false)
+    category = "2023")
 @Solution(
     id = 20231502,
-    name = "Day 15:  - Part 2",
+    name = "Day 15: Lens Library - Part 2",
     url = "https://adventofcode.com/2023/day/15#part2",
-    category = "2023",
-    solved = false)
+    category = "2023")
 public class Day15 extends AOCSolution {
   @Override
   protected void runPartOne(final Scanner in) {
-    run(in);
+    run(in, (map, step) -> calculateHash(step), (map, sum) -> sum);
   }
 
   @Override
   protected void runPartTwo(final Scanner in) {
-    run(in);
+    run(
+        in,
+        (map, step) -> {
+          if (step.contains("=")) {
+            final String[] split = step.split("=");
+            final int hash = calculateHash(split[0]);
+
+            map.computeIfAbsent(hash, key -> new LinkedHashMap<>());
+            map.get(hash).put(split[0], Integer.valueOf(split[1]));
+          } else {
+            final String name = step.substring(0, step.length() - 1);
+            final int hash = calculateHash(name);
+
+            if (map.containsKey(hash)) {
+              map.get(hash).remove(name);
+            }
+          }
+
+          return 0;
+        },
+        (map, sum) -> {
+          long result = 0;
+
+          for (int i = 0; i < 256; ++i) {
+            long boxValue = 0;
+            long lens = 1;
+            for (Map.Entry<String, Integer> boxContents :
+                map.getOrDefault(i, Map.of()).entrySet()) {
+              boxValue += lens * boxContents.getValue();
+              ++lens;
+            }
+
+            result += (i + 1) * boxValue;
+          }
+
+          return result;
+        });
   }
 
-  private void run(final Scanner in) {}
+  private void run(
+      final Scanner in,
+      final BiFunction<Map<Integer, Map<String, Integer>>, String, Integer> stepHandler,
+      final BiFunction<Map<Integer, Map<String, Integer>>, Long, Long> resultHandler) {
+    final Map<Integer, Map<String, Integer>> map = new HashMap<>();
+    long sum = 0;
+
+    final String line = in.nextLine();
+    for (final String step : line.split(",")) {
+      sum += stepHandler.apply(map, step);
+    }
+
+    print(resultHandler.apply(map, sum));
+  }
+
+  private static int calculateHash(final String step) {
+    int hash = 0;
+
+    for (final char character : step.toCharArray()) {
+      hash += character;
+      hash *= 17;
+      hash %= 256;
+    }
+    return hash;
+  }
+
+  private record Lens(String name, int focalLength) {
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) {
+        return true;
+      }
+
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+
+      final Lens lens = (Lens) o;
+      return Objects.equals(name, lens.name);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(name);
+    }
+  }
 }
--- adventofcode/src/test/java/org/ck/adventofcode/year2023/Day15Test.java
+++ adventofcode/src/test/java/org/ck/adventofcode/year2023/Day15Test.java
@@ -1,21 +1,18 @@
 package org.ck.adventofcode.year2023;
 
 import org.ck.adventofcode.util.BaseAOCTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class Day15Test extends BaseAOCTest {
   @ParameterizedTest
   @ValueSource(strings = {"01", "01a"})
-  @Disabled
   void testOne(String name) throws Exception {
     runTest(new Day15()::partOne, "day15/%s".formatted(name));
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"02", "02a"})
-  @Disabled
   void testTwo(String name) throws Exception {
     runTest(new Day15()::partTwo, "day15/%s".formatted(name));
   }

e1f35dc9c27f27824506ede6498ef51fdda5b2a5
TheCK <karsten_knuth@gmx.net>
2023-12-18 18:26:31 +0100

    WIP: AOC 2023 day 18 start

--- adventofcode/src/main/java/org/ck/adventofcode/year2023/Day18.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2023/Day18.java
@@ -1,18 +1,21 @@
 package org.ck.adventofcode.year2023;
 
+import java.util.HashSet;
+import java.util.IntSummaryStatistics;
 import java.util.Scanner;
+import java.util.Set;
 import org.ck.adventofcode.util.AOCSolution;
 import org.ck.codechallengelib.annotation.Solution;
 
 @Solution(
     id = 20231801,
-    name = "Day 18: ",
+    name = "Day 18: Lavaduct Lagoon",
     url = "https://adventofcode.com/2023/day/18",
     category = "2023",
     solved = false)
 @Solution(
     id = 20231802,
-    name = "Day 18:  - Part 2",
+    name = "Day 18: Lavaduct Lagoon - Part 2",
     url = "https://adventofcode.com/2023/day/18#part2",
     category = "2023",
     solved = false)
@@ -27,5 +30,41 @@ public class Day18 extends AOCSolution {
     run(in);
   }
 
-  private void run(final Scanner in) {}
+  private void run(final Scanner in) {
+    final Set<Dig> digs = new HashSet<>();
+    digs.add(new Dig(0, 0));
+    int row = 0;
+    int column = 0;
+
+    while (in.hasNextLine()) {
+      final String[] line = in.nextLine().split(" ");
+      for (int i = 0; i < Integer.parseInt(line[1]); ++i) {
+        switch (line[0]) {
+          case "R" -> ++column;
+          case "L" -> --column;
+          case "U" -> --row;
+          case "D" -> ++row;
+        }
+
+        digs.add(new Dig(row, column));
+      }
+    }
+
+    IntSummaryStatistics rowStats = digs.stream().mapToInt(Dig::row).summaryStatistics();
+
+    int filled = 0;
+    for (int scanRow = rowStats.getMin(); scanRow <= rowStats.getMax(); ++scanRow) {
+      final int finalScanRow = scanRow;
+      final IntSummaryStatistics columnStats =
+          digs.stream()
+              .filter(dig -> dig.row() == finalScanRow)
+              .mapToInt(Dig::column)
+              .summaryStatistics();
+
+      filled += columnStats.getMax() - columnStats.getMin() + 1;
+    }
+    print(filled);
+  }
+
+  private record Dig(int row, int column) {}
 }

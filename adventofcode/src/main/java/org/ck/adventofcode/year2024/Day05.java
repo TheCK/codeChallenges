bd289848edeea409ddd05a51724ea941616577b6
TheCK <karsten_knuth@gmx.net>
2024-12-05 15:55:40 +0100

    aoc 2024 day 5 part 1

--- adventofcode/src/main/java/org/ck/adventofcode/year2024/Day05.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2024/Day05.java
@@ -1,18 +1,18 @@
 package org.ck.adventofcode.year2024;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.ToIntBiFunction;
 import org.ck.adventofcode.util.AOCSolution;
 import org.ck.codechallengelib.annotation.Solution;
 
 @Solution(
     id = 20240501,
-    name = "Day 5: ?",
+    name = "Day 5: Print Queue",
     url = "https://adventofcode.com/2024/day/5",
-    category = "2024",
-    solved = false)
+    category = "2024")
 @Solution(
     id = 20240502,
-    name = "Day 5: ? - Part 2",
+    name = "Day 5: Print Queue - Part 2",
     url = "https://adventofcode.com/2024/day/5#part2",
     category = "2024",
     solved = false)
@@ -20,15 +20,87 @@ public class Day05 extends AOCSolution {
 
   @Override
   protected void runPartOne(final Scanner in) {
-    run(in);
+    run(
+        in,
+        (update, orderRules) -> {
+          if (isValid(update, orderRules)) {
+            return update.get(update.size() / 2);
+          }
+
+          return 0;
+        });
   }
 
   @Override
   protected void runPartTwo(final Scanner in) {
-    run(in);
+    run(
+        in,
+        (update, orderRules) -> {
+          if (!isValid(update, orderRules)) {
+            while (!isValid(update, orderRules)) {
+              break;
+            }
+
+            return update.get(update.size() / 2);
+          }
+
+          return 0;
+        });
   }
 
-  private void run(final Scanner in) {
-    print("Whoopsie");
+  private void run(
+      final Scanner in,
+      final ToIntBiFunction<List<Integer>, Map<Integer, List<Integer>>> getPageNumber) {
+    final Map<Integer, List<Integer>> orderRules = new HashMap<>();
+    final List<List<Integer>> updates = new ArrayList<>();
+
+    readInput(in, orderRules, updates);
+
+    int validMiddlePages = 0;
+    for (List<Integer> update : updates) {
+      validMiddlePages += getPageNumber.applyAsInt(update, orderRules);
+    }
+
+    print(validMiddlePages);
+  }
+
+  private static void readInput(
+      final Scanner in,
+      final Map<Integer, List<Integer>> orderRules,
+      final List<List<Integer>> updates) {
+    while (in.hasNextLine()) {
+      final String line = in.nextLine();
+
+      if (line.contains("|")) {
+        final String[] parts = line.split("\\|");
+
+        orderRules
+            .computeIfAbsent(Integer.valueOf(parts[0]), k -> new ArrayList<>())
+            .add(Integer.valueOf(parts[1]));
+      } else if (!line.isBlank()) {
+        final String[] parts = line.split(",");
+
+        updates.add(Arrays.stream(parts).map(Integer::parseInt).toList());
+      }
+    }
+  }
+
+  private boolean isValid(
+      final List<Integer> update, final Map<Integer, List<Integer>> orderRules) {
+    Set<Integer> alreadyPrinted = new HashSet<>();
+
+    for (Integer page : update) {
+      if (orderRules.containsKey(page)) {
+        for (Integer laterPage : orderRules.get(page)) {
+          if (alreadyPrinted.contains(laterPage)) {
+            return false;
+          }
+        }
+      }
+
+      alreadyPrinted.add(page);
+    }
+
+    return true;
   }
 }
--- adventofcode/src/test/java/org/ck/adventofcode/year2024/Day05Test.java
+++ adventofcode/src/test/java/org/ck/adventofcode/year2024/Day05Test.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@Disabled
 class Day05Test extends BaseAOCTest {
   @ParameterizedTest
   @ValueSource(strings = {"01", "01a"})
@@ -15,6 +14,7 @@ class Day05Test extends BaseAOCTest {
 
   @ParameterizedTest
   @ValueSource(strings = {"02", "02a"})
+  @Disabled
   void testTwo(String name) throws Exception {
     runTest(new Day05()::partTwo, "day05/%s".formatted(name));
   }

9df6b203d3b8d5df6092e0bec9463cd175276d4d
TheCK <karsten_knuth@gmx.net>
2024-12-10 06:39:53 +0100

    aoc 2024 day 10

--- adventofcode/src/main/java/org/ck/adventofcode/year2024/Day10.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2024/Day10.java
@@ -1,34 +1,77 @@
 package org.ck.adventofcode.year2024;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Supplier;
 import org.ck.adventofcode.util.AOCSolution;
 import org.ck.codechallengelib.annotation.Solution;
 
 @Solution(
     id = 20241001,
-    name = "Day 10: ?",
+    name = "Day 10: Hoof It",
     url = "https://adventofcode.com/2024/day/10",
-    category = "2024",
-    solved = false)
+    category = "2024")
 @Solution(
     id = 20241002,
-    name = "Day 10: ? - Part 2",
+    name = "Day 10: Hoof It - Part 2",
     url = "https://adventofcode.com/2024/day/10#part2",
-    category = "2024",
-    solved = false)
+    category = "2024")
 public class Day10 extends AOCSolution {
 
   @Override
   protected void runPartOne(final Scanner in) {
-    run(in);
+    run(in, HashSet::new);
   }
 
   @Override
   protected void runPartTwo(final Scanner in) {
-    run(in);
+    run(in, ArrayList::new);
   }
 
-  private void run(final Scanner in) {
-    print("Whoopsie");
+  private void run(final Scanner in, final Supplier<Collection<Position>> collectionInitializer) {
+    final List<String> grid = new ArrayList<>();
+
+    while (in.hasNextLine()) {
+      grid.add(in.nextLine());
+    }
+
+    long trailHeadCound = 0;
+
+    for (int y = 0; y < grid.size(); ++y) {
+      for (int x = 0; x < grid.get(y).length(); ++x) {
+        if (grid.get(y).charAt(x) == '0') {
+          trailHeadCound += findPaths(grid, x, y, collectionInitializer).size();
+        }
+      }
+    }
+
+    print(trailHeadCound);
   }
+
+  private Collection<Position> findPaths(
+      final List<String> grid,
+      final int x,
+      final int y,
+      final Supplier<Collection<Position>> collectionInitializer) {
+    if (grid.get(y).charAt(x) == '9') {
+      return Set.of(new Position(x, y));
+    }
+
+    final Collection<Position> reachable = collectionInitializer.get();
+    if (x > 0 && grid.get(y).charAt(x - 1) == grid.get(y).charAt(x) + 1) {
+      reachable.addAll(findPaths(grid, x - 1, y, collectionInitializer));
+    }
+    if (x < grid.get(y).length() - 1 && grid.get(y).charAt(x + 1) == grid.get(y).charAt(x) + 1) {
+      reachable.addAll(findPaths(grid, x + 1, y, collectionInitializer));
+    }
+    if (y > 0 && grid.get(y - 1).charAt(x) == grid.get(y).charAt(x) + 1) {
+      reachable.addAll(findPaths(grid, x, y - 1, collectionInitializer));
+    }
+    if (y < grid.size() - 1 && grid.get(y + 1).charAt(x) == grid.get(y).charAt(x) + 1) {
+      reachable.addAll(findPaths(grid, x, y + 1, collectionInitializer));
+    }
+
+    return reachable;
+  }
+
+  record Position(int x, int y) {}
 }
--- adventofcode/src/test/java/org/ck/adventofcode/year2024/Day10Test.java
+++ adventofcode/src/test/java/org/ck/adventofcode/year2024/Day10Test.java
@@ -1,20 +1,18 @@
 package org.ck.adventofcode.year2024;
 
 import org.ck.adventofcode.util.BaseAOCTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@Disabled
 class Day10Test extends BaseAOCTest {
   @ParameterizedTest
-  @ValueSource(strings = {"01", "01a"})
+  @ValueSource(strings = {"01", "01a", "01b", "01c", "01d"})
   void testOne(String name) throws Exception {
     runTest(new Day10()::partOne, "day10/%s".formatted(name));
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"02", "02a"})
+  @ValueSource(strings = {"02", "02a", "02b", "02c", "02d"})
   void testTwo(String name) throws Exception {
     runTest(new Day10()::partTwo, "day10/%s".formatted(name));
   }

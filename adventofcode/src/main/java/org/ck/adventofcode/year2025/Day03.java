a701dc8f6f1a43ac9f2b9fdcac71e8cee2e3c3b7
TheCK <karsten_knuth@gmx.net>
2025-12-03 18:56:21 +0100

    AOC 2025 day 3

--- adventofcode/src/main/java/org/ck/adventofcode/year2025/Day03.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2025/Day03.java
@@ -6,29 +6,53 @@ import org.ck.codechallengelib.annotation.Solution;
 
 @Solution(
     id = 20250301,
-    name = "Day 3: ",
+    name = "Day 3: Lobby ",
     url = "https://adventofcode.com/2025/day/3",
-    category = "2025",
-    solved = false)
+    category = "2025")
 @Solution(
     id = 20250302,
-    name = "Day 3:  - Part 2",
+    name = "Day 3: Lobby  - Part 2",
     url = "https://adventofcode.com/2025/day/3#part2",
-    category = "2025",
-    solved = false)
+    category = "2025")
 public class Day03 extends AOCSolution {
 
   @Override
   protected void runPartOne(final Scanner in) {
-    run(in);
+    run(in, 2);
   }
 
   @Override
   protected void runPartTwo(final Scanner in) {
-    run(in);
+    run(in, 12);
   }
 
-  private void run(final Scanner in) {
-    // not yet implemented
+  private void run(final Scanner in, final int numberOfBatteries) {
+    long jolts = 0;
+
+    while (in.hasNextLine()) {
+      final String line = in.nextLine();
+
+      long currentJolts = 0;
+      int startIndex = 0;
+      for (int battery = numberOfBatteries - 1; battery >= 0; --battery) {
+        int maxValue = 0;
+        int maxIndex = startIndex;
+        for (int i = startIndex; i < line.length() - battery; ++i) {
+          final int value = line.charAt(i) - '0';
+
+          if (value > maxValue) {
+            maxValue = value;
+            maxIndex = i;
+          }
+        }
+
+        startIndex = maxIndex + 1;
+        currentJolts += maxValue * (long) Math.pow(10, battery);
+      }
+
+      jolts += currentJolts;
+    }
+
+    print(jolts);
   }
 }

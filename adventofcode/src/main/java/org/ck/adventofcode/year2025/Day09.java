eb0baa9235676aa942fbd164466810c8ae94cbf7
TheCK <karsten_knuth@gmx.net>
2025-12-09 21:31:09 +0100

    AOC 2025 day 9

--- adventofcode/src/main/java/org/ck/adventofcode/year2025/Day09.java
+++ adventofcode/src/main/java/org/ck/adventofcode/year2025/Day09.java
@@ -6,16 +6,14 @@ import org.ck.codechallengelib.annotation.Solution;
 
 @Solution(
     id = 20250901,
-    name = "Day 9: ",
+    name = "Day 9: Movie Theater",
     url = "https://adventofcode.com/2025/day/9",
-    category = "2025",
-    solved = false)
+    category = "2025")
 @Solution(
     id = 20250902,
-    name = "Day 9:  - Part 2",
+    name = "Day 9: Movie Theater - Part 2",
     url = "https://adventofcode.com/2025/day/9#part2",
-    category = "2025",
-    solved = false)
+    category = "2025")
 public class Day09 extends AOCSolution {
 
   @Override
@@ -27,83 +25,36 @@ public class Day09 extends AOCSolution {
   protected void runPartTwo(final Scanner in) {
     run(
         in,
-        (current, other, boundingLines) -> {
+        (current, other, lines) -> {
           final long minX = Math.min(current.x, other.x);
           final long maxX = Math.max(current.x, other.x);
           final long minY = Math.min(current.y, other.y);
           final long maxY = Math.max(current.y, other.y);
 
-          final Line topLine = new Line(new Coordinate(minX, minY), new Coordinate(maxX, minY));
-          final Line bottomLine = new Line(new Coordinate(minX, maxY), new Coordinate(maxX, maxY));
-          final Line leftLine = new Line(new Coordinate(minX, minY), new Coordinate(minX, maxY));
-          final Line rightLine = new Line(new Coordinate(maxX, minY), new Coordinate(maxX, maxY));
+          final Line top =
+              new Line(new Coordinate(minX + 1, minY + 1), new Coordinate(maxX - 1, minY + 1));
+          final Line bottom =
+              new Line(new Coordinate(minX + 1, maxY - 1), new Coordinate(maxX - 1, maxY - 1));
+          if (lines.stream().anyMatch(top::intersects)
+              || lines.stream().anyMatch(bottom::intersects)) {
+            return false;
+          }
 
-          return boundingLines.stream()
-              .anyMatch(
-                  line ->
-                      topLine.intersectsTopLine(line)
-                          || bottomLine.intersectsBottomLine(line)
-                          || leftLine.intersectsLeftLine(line)
-                          || rightLine.intersectsRightLine(line));
-
-          /*
-          final Coordinate topCorner =
-              current.y < other.y
-                  ? new Coordinate(other.x, current.y)
-                  : new Coordinate(current.x, other.y);
-          final Coordinate bottomCorner =
-              current.y >= other.y
-                  ? new Coordinate(other.x, current.y)
-                  : new Coordinate(current.x, other.y);
-
-          final boolean allPointsInside =
-              isInsideBelow(topCorner, boundingLines) && isInsideAbove(bottomCorner, boundingLines);
-
-          if (!allPointsInside) {
+          final Line left =
+              new Line(new Coordinate(minX + 1, minY + 1), new Coordinate(minX + 1, maxY - 1));
+          final Line right =
+              new Line(new Coordinate(maxX - 1, minY + 1), new Coordinate(maxX - 1, maxY - 1));
+          if (lines.stream().anyMatch(left::intersects)
+              || lines.stream().anyMatch(right::intersects)) {
             return false;
           }
 
           return true;
-
-          /*
-          for (long x = Math.min(current.x, other.x) + 1; x < Math.max(current.x, other.x); ++x) {
-            if (!isInside(new Coordinate(x, current.y), boundingLines)
-                || !isInside(new Coordinate(x, other.y), boundingLines)) {
-              return false;
-            }
-          }
-
-          for (long y = Math.min(current.y, other.y) + 1; y < Math.max(current.y, other.y); ++y) {
-            if (!isInside(new Coordinate(current.x, y), boundingLines)
-                || !isInside(new Coordinate(other.x, y), boundingLines)) {
-              return false;
-            }
-          }
-
-          return true;
-
-          /*final Set<Line> rectangle =
-              Set.of(
-                  new Line(current, corner1),
-                  new Line(current, corner2),
-                  new Line(other, corner1),
-                  new Line(other, corner2));
-
-          for (final Line side : rectangle) {
-            for (final Line boundingLine : boundingLines) {
-              if (side.reallyIntersects(boundingLine)) {
-                return false;
-              }
-            }
-          }
-
-          return true;*/
         });
   }
 
-  private void run(final Scanner in, final TriPredicate validityChecker) {
+  private void run(final Scanner in, final ValidityChecker validityChecker) {
     final List<Coordinate> tiles = new ArrayList<>();
-    long maxArea = 0;
 
     while (in.hasNextLine()) {
       final String[] line = in.nextLine().split(",");
@@ -115,12 +66,9 @@ public class Day09 extends AOCSolution {
       lines.add(new Line(tiles.get(i), tiles.get((i + 1) % tiles.size())));
     }
 
-    for (int i = 0; i < tiles.size(); ++i) {
-      final Coordinate current = tiles.get(i);
-
-      for (int j = i + 1; j < tiles.size(); ++j) {
-        final Coordinate other = tiles.get(j);
-
+    long maxArea = 0;
+    for (final Coordinate current : tiles) {
+      for (final Coordinate other : tiles) {
         final long area = (1 + Math.abs(other.x - current.x)) * (1 + Math.abs(other.y - current.y));
         if (area > maxArea && validityChecker.apply(current, other, lines)) {
           maxArea = area;
@@ -131,100 +79,31 @@ public class Day09 extends AOCSolution {
     print(maxArea);
   }
 
-  private boolean isInsideBelow(final Coordinate point, final List<Line> boundingLines) {
-    final Line ray = new Line(point, new Coordinate(100000, point.y));
-
-    return boundingLines.stream().filter(ray::intersectsRayBelow).count() % 2 == 1;
-  }
-
-  private boolean isInsideAbove(final Coordinate point, final List<Line> boundingLines) {
-    final Line ray = new Line(point, new Coordinate(100000, point.y));
-
-    return boundingLines.stream().filter(ray::intersectsRayAbove).count() % 2 == 1;
-  }
-
-  private interface TriPredicate {
-    boolean apply(Coordinate tile1, Coordinate tile2, List<Line> lines);
+  private interface ValidityChecker {
+    boolean apply(Coordinate tile1, Coordinate tile2, final List<Line> lines);
   }
 
   private record Coordinate(long x, long y) {}
 
   private record Line(Coordinate start, Coordinate end) {
-    public boolean intersectsTopLine(final Line other) {
+    public boolean intersects(final Line other) {
+      final boolean thisIsVertical = start.x == end.x;
       final boolean otherIsVertical = other.start.x == other.end.x;
 
-      if (!otherIsVertical) {
+      if (thisIsVertical && otherIsVertical || !thisIsVertical && !otherIsVertical) {
         return false;
       }
 
-      return Math.min(start.x, end.x) < other.start.x
-          && other.start.x < Math.max(start.x, end.x)
-          && Math.min(other.start.y, other.end.y) <= start.y
-          && start.y < Math.max(other.start.y, other.end.y);
-    }
-
-    public boolean intersectsBottomLine(final Line other) {
-      final boolean otherIsVertical = other.start.x == other.end.x;
-
-      if (!otherIsVertical) {
-        return false;
-      }
-
-      return Math.min(start.x, end.x) < other.start.x
-          && other.start.x < Math.max(start.x, end.x)
-          && Math.min(other.start.y, other.end.y) < start.y
-          && start.y <= Math.max(other.start.y, other.end.y);
-    }
-
-    public boolean intersectsLeftLine(final Line other) {
-      final boolean otherIsVertical = other.start.x == other.end.x;
-
-      if (otherIsVertical) {
-        return false;
+      if (thisIsVertical) {
+        return Math.min(other.start.x, other.end.x) < start.x
+            && start.x < Math.max(other.start.x, other.end.x)
+            && Math.min(start.y, end.y) <= other.start.y
+            && other.start.y < Math.max(start.y, end.y);
       }
 
       return Math.min(start.x, end.x) <= other.start.x
-          && other.start.x < Math.max(start.x, end.x)
-          && Math.min(other.start.y, other.end.y) < start.y
-          && start.y < Math.max(other.start.y, other.end.y);
-    }
-
-    public boolean intersectsRightLine(final Line other) {
-      final boolean otherIsVertical = other.start.x == other.end.x;
-
-      if (otherIsVertical) {
-        return false;
-      }
-
-      return Math.min(start.x, end.x) < other.start.x
-          && other.start.x <= Math.max(start.x, end.x)
-          && Math.min(other.start.y, other.end.y) < start.y
-          && start.y < Math.max(other.start.y, other.end.y);
-    }
-
-    public boolean intersectsRayBelow(final Line other) {
-      final boolean otherIsVertical = other.start.x == other.end.x;
-
-      if (!otherIsVertical) {
-        return false;
-      }
-
-      return Math.min(start.x, end.x) < other.start.x
           && other.start.x <= Math.max(start.x, end.x)
           && Math.min(other.start.y, other.end.y) <= start.y
-          && start.y < Math.max(other.start.y, other.end.y);
-    }
-
-    public boolean intersectsRayAbove(final Line other) {
-      final boolean otherIsVertical = other.start.x == other.end.x;
-
-      if (!otherIsVertical) {
-        return false;
-      }
-
-      return Math.min(start.x, end.x) < other.start.x
-          && other.start.x <= Math.max(start.x, end.x)
-          && Math.min(other.start.y, other.end.y) < start.y
           && start.y <= Math.max(other.start.y, other.end.y);
     }
   }
